10b20314c110400d1a3c05743a227da6b0971a9a
Ken <bigdstut@gmail.com>
2016-07-26 15:08:23 +0800

    init project

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "ready!" );
+    backgroundAnimation();
+    // $(window).resize(function(){
+    // 	backgroundAnimation();
+    
+    // });
+});
